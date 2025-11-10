244daaec0c264d6472f3b6be022da655d17e6c1d
Barbara Kloss Furquim <barbarakf383@gmail.com>
2025-11-10 20:25:38 -0300

    ExerciseController finalizado

---
+++ src/ExerciseController/ExerciseFormController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Systemfy\App\ExerciseController;
+
+use Systemfy\App\Controller\Controller;
+use Systemfy\App\Repository\ExerciseRepository;
+
+$caminho = __DIR__ . '/../../../databaselocal';
+$pdo = new \PDO("mysql:$caminho");
+
+class ExerciseFormController implements Controller
+{
+
+    function __construct(private ExerciseRepository $exerciseRepository)
+    {
+
+    }
+
+    public function processaRequisicao(): void
+    {
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+        $exercise = null;
+        if ($id !== false && $id !== null) {
+            $exercise = $this->exerciseRepository->find($id);
+        }
+        require_once __DIR__ . '/../../views/formsbook.php';
+    }
+}
