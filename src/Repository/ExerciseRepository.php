eff50534a3e6f4d604d5e13f1c5735e82a229ac0
Barbara Kloss Furquim <barbarakf383@gmail.com>
2025-11-11 18:57:27 -0300

    Update ExerciseRepository.php

--- src/Repository/ExerciseRepository.php
+++ src/Repository/ExerciseRepository.php
@@ -12,7 +12,7 @@ class ExerciseRepository
 
     public function add(Exercise $exercise) : bool
     {
-        $sql = "INSERT INTO exercise(id_user, peso, tempo_descanso, repeticao, tipo_exercicio, objetivo, dia, observacao) values(?,?,?,?,?,?,?,?)";
+        $sql = "INSERT INTO exercise(id_user, peso, tempo_descanso, repeticao, tipo_exercicio, objetivo, dia, observacao, categoria, id_personal) values(?,?,?,?,?,?,?,?,?,?);";
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(1, $exercise->id_user);
         $stmt->bindValue(2, $exercise->peso);
@@ -22,6 +22,8 @@ class ExerciseRepository
         $stmt->bindValue(6, $exercise->objetivo);
         $stmt->bindValue(7, $exercise->dia);
         $stmt->bindValue(8, $exercise->observacao);
+        $stmt->bindValue(9, $exercise->categoria);
+        $stmt->bindValue(10, $exercise->id_personal);
 
         $result = $stmt->execute();
         $id = $this->pdo->lastInsertId();
@@ -43,7 +45,7 @@ class ExerciseRepository
         $sql = 'UPDATE exercise SET peso = :peso, 
         tempo_descanso= :tempo_descanso, repeticao= :repeticao,
         tipo_exercicio = :tipo_exercicio, objetivo = :objetivo, 
-        dia = :dia, observacao = :observacao  WHERE id = :id;';
+        dia = :dia, observacao = :observacao, categoria = :categoria  WHERE id = :id;';
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':peso', $exercise->peso);
         $stmt->bindValue(':tempo_descanso', $exercise->tempo_descanso);
@@ -52,6 +54,7 @@ class ExerciseRepository
         $stmt->bindValue(':objetivo', $exercise->objetivo);
         $stmt->bindValue(':dia', $exercise->dia);
         $stmt->bindValue(':observacao', $exercise->observacao);
+        $stmt->bindValue(':categoria', $exercise->categoria);
         $stmt->bindValue(':id', $exercise->id);
 
         return $stmt->execute();
@@ -79,13 +82,16 @@ class ExerciseRepository
 
     public function hydrateExercise(array $ExerciseData): Exercise
     {
-        $exercise = new Exercise($ExerciseData['peso'],
+        $exercise = new Exercise( $ExerciseData['id_user'],
+            $ExerciseData['peso'],
             $ExerciseData['tempo_descanso'],
             $ExerciseData['repeticao'],
             $ExerciseData['tipo_exercicio'],
             $ExerciseData['objetivo'],
             $ExerciseData['dia'],
-            $ExerciseData['observacao']);
+            $ExerciseData['observacao'],
+            $ExerciseData['categoria'],
+            $ExerciseData['id_personal']);
         $exercise->setId($ExerciseData['id']);
         return $exercise;
     }
